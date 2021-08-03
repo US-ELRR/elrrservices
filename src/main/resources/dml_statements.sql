7f1dadcaf4b791ad2f3bf604de46f4641c66637c
arun darpally <adarpally@deloitte.com>
2021-08-03 16:49:47 -0400

    adding modified dml statements

--- src/main/resources/dml_statements.sql
+++ src/main/resources/dml_statements.sql
@@ -19,4 +19,54 @@ INSERT INTO imports
 (importid, importname, importstartdate, importenddate, recordstatus, updatedby, inserteddate, lastmodified)
 VALUES(1, 'Deloitte LRS', '2021-07-19', '2021-07-19', 'INPROCESS', NULL, NULL, '2021-07-19 16:33:00.018');
 
+--competency table
 
+delete from competency;
+
+INSERT INTO competency
+(competencyid, competencydefinitionidentifier, competencydefinitionidentifierurl, competencytaxonomyid, competencydefinitionvalidstartdate, competencydefinitionvalideenddate, competencydefinitionparentidentifier, competencydefinitionparenturl, competencydescriptionparentcode, competencydefinitioncode, competencydefinitiontype, competencydefinitiontypeurl, competencydefinitionstatement, recordstatus, updatedby, inserteddate, lastmodified, competencyframeworktitle, competencyframeworkversion, competencyframeworkidentifier, competencyframeworkdescription, competencyframeworksubject, competencyframeworkvalidstartdate, competencyframeworkvalidenddate)
+VALUES(100, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Skill and Roles: Business Skills and Acumen', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO competency
+(competencyid, competencydefinitionidentifier, competencydefinitionidentifierurl, competencytaxonomyid, competencydefinitionvalidstartdate, competencydefinitionvalideenddate, competencydefinitionparentidentifier, competencydefinitionparenturl, competencydescriptionparentcode, competencydefinitioncode, competencydefinitiontype, competencydefinitiontypeurl, competencydefinitionstatement, recordstatus, updatedby, inserteddate, lastmodified, competencyframeworktitle, competencyframeworkversion, competencyframeworkidentifier, competencyframeworkdescription, competencyframeworksubject, competencyframeworkvalidstartdate, competencyframeworkvalidenddate)
+VALUES(101, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Contract Principles: General Contracting Concepts', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO competency
+(competencyid, competencydefinitionidentifier, competencydefinitionidentifierurl, competencytaxonomyid, competencydefinitionvalidstartdate, competencydefinitionvalideenddate, competencydefinitionparentidentifier, competencydefinitionparenturl, competencydescriptionparentcode, competencydefinitioncode, competencydefinitiontype, competencydefinitiontypeurl, competencydefinitionstatement, recordstatus, updatedby, inserteddate, lastmodified, competencyframeworktitle, competencyframeworkversion, competencyframeworkidentifier, competencyframeworkdescription, competencyframeworksubject, competencyframeworkvalidstartdate, competencyframeworkvalidenddate)
+VALUES(102, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'SERVICE3', NULL, NULL, NULL, NULL, NULL, NULL);
+
+---
+
+delete from role;
+
+
+INSERT INTO role
+(roleid, rolename, status, updatedby, inserteddate, lastmodified)
+VALUES(1, 'TRAINING_MANAGER    ', 'Y         ', NULL, NULL, NULL);
+INSERT INTO role
+(roleid, rolename, status, updatedby, inserteddate, lastmodified)
+VALUES(2, 'CAREER_MANAGER      ', 'Y         ', NULL, NULL, NULL);
+INSERT INTO role
+(roleid, rolename, status, updatedby, inserteddate, lastmodified)
+VALUES(3, 'LEARNER             ', 'Y         ', NULL, NULL, NULL);
+
+---
+
+delete from rolerelations;
+
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(1, 106, 3, 101, 'Y         ', NULL, NULL, NULL, 2, 1);
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(1, 106, 3, 100, 'Y         ', NULL, NULL, NULL, 2, 2);
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(1, 106, 3, 102, 'Y         ', NULL, NULL, NULL, 2, 3);
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(1, 106, 3, 104, 'Y         ', NULL, NULL, NULL, 2, 6);
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(2, 105, 3, 104, 'Y         ', NULL, NULL, NULL, 2, 5);
+INSERT INTO rolerelations
+(parentroleid, parentpersonid, childroleid, childpersonid, status, updatedby, inserteddate, lastmodified, id, id1)
+VALUES(2, 105, 3, 100, 'Y         ', NULL, NULL, NULL, NULL, 4);
