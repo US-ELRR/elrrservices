653331cd659ed9ed64638ffa2e95184a14ca5b34
Mohan <mnelakurti@deloitte.com>
2023-02-03 15:24:32 -0500

    Added junit cases from P1 to github

---
+++ src/test/java/com/deloitte/elrr/entity/PersonnelTest.java
@@ -0,0 +1,57 @@
+package com.deloitte.elrr.entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author mnelakurti
+ *
+ */
+@ExtendWith(MockitoExtension.class)
+public class PersonnelTest {
+    @Test
+    void testGetContactInformation() {
+
+    }
+
+    @Test
+    void testGetEmployment() {
+
+    }
+
+    @Test
+    void testGetOrganization() {
+
+    }
+
+    @Test
+    void testGetPerson() {
+
+    }
+
+    @Test
+    void testSetContactInformation() {
+
+    }
+
+    @Test
+    void testSetEmployment() {
+
+    }
+
+    @Test
+    void testSetOrganization() {
+
+    }
+
+    @Test
+    void testSetPerson() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
