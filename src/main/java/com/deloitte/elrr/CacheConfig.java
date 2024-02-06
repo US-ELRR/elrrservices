ce12f74744459a5536e2f212b986392488e138d5
travipati <travipati@deloitte.com>
2024-02-06 12:13:03 -0600

    file match gitlab

--- src/main/java/com/deloitte/elrr/CacheConfig.java
+++ src/main/java/com/deloitte/elrr/CacheConfig.java
@@ -1,7 +1,9 @@
 package com.deloitte.elrr;
 
-import org.springframework.context.annotation.Bean;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,17 +13,22 @@ import org.springframework.security.saml2.provider.service.registration.RelyingP
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class CacheConfig {
 
-	//@Value("${lrs.samlid}")
-	private String samlid;// = "elrrsamltest";
-	//@Value("${lrs.samlurl}")
-	private String samlurl;// = "https://idp.ssocircle.com";
+    /**
+     * id of IDP for SAML connection.
+     */
+    @Value("${lrs.samlid}")
+    private String samlid;
 
+    /**
+     * url of IDP metadata for SAML connection.
+     */
+    @Value("${lrs.samlurl}")
+    private String samlurl;
 
     /**
      * access IDP using lrs.samlurl and lrs.samlid application properties.
@@ -31,15 +38,15 @@ public class CacheConfig {
     @Bean
     public RelyingPartyRegistrationRepository relyingPartyRegistrations()
             throws Exception {
-        // if (samlid.isEmpty() || samlurl.isEmpty()) {
-                return null;
-        // }
-        // RelyingPartyRegistration relyingPartyRegistration =
-        // RelyingPartyRegistrations
-        // .fromMetadataLocation(samlurl).registrationId(samlid).build();
+        if (samlid.isEmpty() || samlurl.isEmpty()) {
+            return null;
+        }
+        RelyingPartyRegistration relyingPartyRegistration =
+                RelyingPartyRegistrations
+                        .fromMetadataLocation(samlurl).registrationId(samlid).build();
 
-        // return new InMemoryRelyingPartyRegistrationRepository(
-        //         relyingPartyRegistration);
+        return new InMemoryRelyingPartyRegistrationRepository(
+                relyingPartyRegistration);
     }
 
     /**
@@ -51,28 +58,26 @@ public class CacheConfig {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http)
             throws Exception {
-
-        // if (samlid.isEmpty() || samlurl.isEmpty()) {
+        if (samlid.isEmpty() || samlurl.isEmpty()) {
             http.authorizeHttpRequests(
                     authorize -> authorize.anyRequest().authenticated());
             http.formLogin(withDefaults());
-        // } else {
-        //     http.authorizeHttpRequests(
-        //         authorize -> authorize.anyRequest().authenticated())
-        //             .saml2Metadata(withDefaults())
-        //             .saml2Login(saml2 -> {
-        //                 try {
-        //                     saml2
-        //                             .relyingPartyRegistrationRepository(
-        //                                     relyingPartyRegistrations());
-		//						saml2.successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
-        //                 } catch (Exception e) {
-        //                     e.printStackTrace();
-        //                 }
-        //             });
-        // }
+        } else {
+            http.authorizeHttpRequests(
+                            authorize -> authorize.anyRequest().authenticated())
+                    .saml2Metadata(withDefaults())
+                    .saml2Login(saml2 -> {
+                        try {
+                            saml2
+                                    .relyingPartyRegistrationRepository(
+                                            relyingPartyRegistrations());
+                            saml2.successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
 
         return http.build();
-
     }
-}
+
