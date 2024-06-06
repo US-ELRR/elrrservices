3e8e0eb4706f3b14db511f51f760b4c6914e3af1
JDTobin <90791836+JDTobin@users.noreply.github.com>
2024-06-06 15:36:19 -0400

    comment out checks

--- src/main/java/com/deloitte/elrr/HeaderFilter.java
+++ src/main/java/com/deloitte/elrr/HeaderFilter.java
@@ -10,15 +10,17 @@ import java.io.IOException;
 @Component
 public class HeaderFilter implements Filter {
     @Override
-    public
-    void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
-        if("https".equalsIgnoreCase(httpServletRequest.getHeader("X-Forwarded-Proto"))) {
-            chain.doFilter(request, response);
-        }else {
-            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, "Not a HTTPS request.");
-        }
+        // if("https".equalsIgnoreCase(httpServletRequest.getHeader("X-Forwarded-Proto")))
+        // {
+        chain.doFilter(request, response);
+        // }else {
+        // ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN,
+        // "Not a HTTPS request.");
+        // }
     }
 }
--- src/main/java/com/deloitte/elrr/JSONRequestSizeLimitFilter.java
+++ src/main/java/com/deloitte/elrr/JSONRequestSizeLimitFilter.java
@@ -14,20 +14,23 @@ import java.io.IOException;
 @Component
 public class JSONRequestSizeLimitFilter extends OncePerRequestFilter {
     private static final long MAX_SIZE_LIMIT = 2000000;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response, FilterChain filterChain)
+            HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         if (isApplicationJson(request) && request.getContentLengthLong() < MAX_SIZE_LIMIT) {
             filterChain.doFilter(request, response);
-        }else {
-            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST, "Request size exceeds the limit.");
+        } else {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST,
+                    "Request size exceeds the limit.");
         }
     }
 
     private boolean isApplicationJson(HttpServletRequest httpRequest) {
-        return (MediaType.APPLICATION_JSON.isCompatibleWith(MediaType
-                .parseMediaType(httpRequest.getHeader(HttpHeaders.CONTENT_TYPE))));
+        // return (MediaType.APPLICATION_JSON.isCompatibleWith(MediaType
+        // .parseMediaType(httpRequest.getHeader(HttpHeaders.CONTENT_TYPE))));
+        return true;
     }
 
 }
