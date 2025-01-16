27d5ac82d654f71fe8da9fb07fca38a069b9c0ff
Leven <phleven@deloitte.com>
2025-01-16 09:08:18 -0500

    Add try/catch, enhance error nmessages and remove unneeded dependencies.

--- src/main/java/com/deloitte/elrr/JSONRequestSizeLimitFilter.java
+++ src/main/java/com/deloitte/elrr/JSONRequestSizeLimitFilter.java
@@ -8,18 +8,20 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+// import com.deloitte.elrr.elrrconsolidate.JSONRequestSizeLimitFilter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class JSONRequestSizeLimitFilter extends OncePerRequestFilter {
 
   @Value("${json.max.size.limit}")
-  private long maxSizeLimit;
-
-  private long MAX_SIZE_LIMIT = maxSizeLimit;
+  private long MAX_SIZE_LIMIT;
 
   @Value("${check.media.type.json}")
   private boolean checkMediaTypeJson;
@@ -28,11 +30,17 @@ public class JSONRequestSizeLimitFilter extends OncePerRequestFilter {
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    if (isApplicationJson(request) && request.getContentLengthLong() < MAX_SIZE_LIMIT) {
-      filterChain.doFilter(request, response);
-    } else {
-      ((HttpServletResponse) response)
-          .sendError(HttpServletResponse.SC_BAD_REQUEST, "Request size exceeds the limit.");
+    try {
+      if (isApplicationJson(request) && request.getContentLengthLong() < MAX_SIZE_LIMIT) {
+        filterChain.doFilter(request, response);
+      } else {
+        log.error("Request size exceeds the limit.");
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Request size exceeds the limit.");
+      }
+    } catch (IOException | ServletException e) {
+      log.error("Error: " + e.getMessage());
+      e.printStackTrace();
+      return;
     }
   }
 
