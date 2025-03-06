aed10d99843e076d13e0677db59e5f0a4e56bda5
Cliff Casey <cliff@yetanalytics.com>
2025-03-06 18:32:44 -0500

    mil record dto lengths

--- src/main/java/com/deloitte/elrr/dto/MilitaryRecordDto.java
+++ src/main/java/com/deloitte/elrr/dto/MilitaryRecordDto.java
@@ -15,39 +15,39 @@ import lombok.RequiredArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 public class MilitaryRecordDto extends AbstractDto {
 
-    @Size(max = 100)
+    @Size(max = 255)
     @NotNull
     private String branch;
 
-    @Size(max = 100)
+    @Size(max = 255)
     @NotNull
     private String country;
 
     private LocalDate inductionDate;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String inductionRank;
 
     private LocalDate releaseDate;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String currentRank;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String currentStatus;
 
     private LocalDate dischargeDate;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String dischargeCategory;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String dischargeRank;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String highestRank;
 
-    @Size(max = 100)
+    @Size(max = 255)
     private String militaryId;
 
 }
