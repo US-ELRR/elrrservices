1c2c20ad9d3b91a89717a5416fb5e4a6ff0b3e23
Milton Reder <milt@yetanalytics.com>
2025-06-10 08:52:04 -0400

    add javadoc for toMap

--- src/main/java/com/deloitte/elrr/services/dto/PermissionDto.java
+++ src/main/java/com/deloitte/elrr/services/dto/PermissionDto.java
@@ -37,6 +37,11 @@ public class PermissionDto extends AbstractDto {
         DISASSOCIATE
     }
 
+    /**
+     * Converts this PermissionDto to a Map representation.
+     *
+     * @return a Map containing the resource, resourceId, and actions.
+     */
     public Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
         map.put("resource", this.resource);
