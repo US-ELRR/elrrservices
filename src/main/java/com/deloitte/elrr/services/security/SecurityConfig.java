c09c751d63d69f84899871cbdbd7b5c449e3d43e
Milton Reder <milt@yetanalytics.com>
2025-06-17 14:36:31 -0400

    poke a hole for swagger

--- src/main/java/com/deloitte/elrr/services/security/SecurityConfig.java
+++ src/main/java/com/deloitte/elrr/services/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/ping").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/**")
                             .hasRole("API")
                         .requestMatchers("/admin/**")
